8a00c8a9995b621af8b10e13c8c8fa0c97a37999
Vibhor Singh <vibhorsinghcs@gmail.com>
2022-06-29 22:35:36 +0530

    Added 11 Math Operator

---
+++ 01-JS Basics/11-MathOperator.js
@@ -0,0 +1,13 @@
+/* Write a function that takes 6 values (a,b,c,d,e,f) as arguments
+Sum a and b
+Then substract by c
+Then multiply by d and divide by e
+Finally raise to the power of f and return the result
+Tipp: mind the order */
+
+function myFunction(a, b, c, d, e, f) {
+  return Math.pow(((a + b - c) * d) / e, f);
+}
+
+console.log(myFunction(6, 5, 4, 3, 2, 1));
+console.log(myFunction(6, 2, 1, 4, 2, 3));
