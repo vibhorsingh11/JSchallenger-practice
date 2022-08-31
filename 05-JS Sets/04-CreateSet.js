ad4b229004426b4d16093c2e0c4691702d5fb8a7
Vibhor Singh <vibhorsinghcs@gmail.com>
2022-08-31 14:20:36 +0530

    Added 04 Create Set

---
+++ 05-JS Sets/04-CreateSet.js
@@ -0,0 +1,9 @@
+/* Write a function that takes three elements of any type as arguments
+Create a Set from those elements
+Return the result */
+
+function myFunction(a, b, c) {
+  return new Set([a, b, c]);
+}
+
+console.log(myFunction(1, "b", 3));
