456e6ab58027e4031e75e5742437e547ac473e7d
Vibhor Singh <vibhorsinghcs@gmail.com>
2022-09-04 21:45:37 +0530

    Added 01 Execute Function

---
+++ 07-Events/01-ExecuteFunction.js
@@ -0,0 +1,12 @@
+/* The Javascript function handleText fills the input field with the words Hello World. But, there is no code to execute this function.
+Complete the existing code below such that the function is called when the button is clicked. Verify by clicking the button. */
+
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+
+const handleClick = () => {
+  input.value = "Hello World";
+};
+
+// type in your code here
+button.addEventListener("click", handleClick);
